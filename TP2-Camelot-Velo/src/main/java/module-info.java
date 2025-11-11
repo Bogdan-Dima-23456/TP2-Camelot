fff01498ccf69109ee76827124dad96b0285078e
Bogdan-Dima-23456 <riciubondaru@gmail.com>
2025-11-11 14:49:23 -0500

    Ajout du TP

---
+++ TP2-Camelot-Velo/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module bdeb.qc.ca.sim.tp2camelotvelo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens bdeb.qc.ca.sim.tp2camelotvelo to javafx.fxml;
+    exports bdeb.qc.ca.sim.tp2camelotvelo;
+}
