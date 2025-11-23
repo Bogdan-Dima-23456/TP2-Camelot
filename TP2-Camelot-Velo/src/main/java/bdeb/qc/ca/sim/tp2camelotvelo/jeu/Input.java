2cf7edd4b3b569316e73c5bbb1cfdfeb64e81a07
Bogdan-Dima-23456 <riciubondaru@gmail.com>
2025-11-23 18:11:33 -0500

    Classe Input complétée

---
+++ TP2-Camelot-Velo/src/main/java/bdeb/qc/ca/sim/tp2camelotvelo/jeu/Input.java
@@ -0,0 +1,24 @@
+package bdeb.qc.ca.sim.tp2camelotvelo.jeu;
+
+import javafx.scene.input.KeyCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Input {
+
+    // Ensemble des touches actuellement appuyées
+    private static Set<KeyCode> touches = new HashSet<>();
+
+    public static boolean isKeyPressed(KeyCode code){
+        return touches.contains(code);
+    }
+
+    public static void setKeyPressed(KeyCode code,boolean appuie){
+        if(appuie){
+            touches.add(code);}
+
+        else{
+            touches.remove(code);}
+    }
+}
