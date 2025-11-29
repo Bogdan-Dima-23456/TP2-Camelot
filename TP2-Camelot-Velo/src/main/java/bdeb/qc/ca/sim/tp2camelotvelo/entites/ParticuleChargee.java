074a03d0606cd366fb06e34dcc89f7aa56fe0837
MK <mikakort007@gmail.com>
2025-11-29 12:20:47 -0500

    cleanup

--- TP2-Camelot-Velo/src/main/java/bdeb/qc/ca/sim/tp2camelotvelo/entites/ParticuleChargee.java
+++ TP2-Camelot-Velo/src/main/java/bdeb/qc/ca/sim/tp2camelotvelo/entites/ParticuleChargee.java
@@ -1,12 +1,9 @@
 package bdeb.qc.ca.sim.tp2camelotvelo.entites;
 
 import bdeb.qc.ca.sim.tp2camelotvelo.jeu.Camera;
-import bdeb.qc.ca.sim.tp2camelotvelo.jeu.UtilitairesDessins;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
-
-import java.util.List;
 
 // Classe pour les particules chargées qui génèrent un champ électrique
 // Les journaux sont affectés par ce champ
@@ -46,54 +43,5 @@ public class ParticuleChargee extends ObjetDeJeu {
     // Getter pour récupérer la charge
     public double getCharge() {
         return CHARGE;
-    }
-
-    // Calcule le champ électrique total à la position donnée
-    public static Point2D champElectrique(List<ParticuleChargee> particules, Point2D positionMonde) {
-        Point2D champTotal = Point2D.ZERO;
-        double K = 8.99e9; // Constante de Coulomb (N⋅m²/C²)
-        
-        for (ParticuleChargee particule : particules) {
-            Point2D direction = positionMonde.subtract(particule.getPosition());
-            double distance = direction.magnitude();
-            
-            if (distance > 0.1) { // Éviter division par zéro
-                double E = K * particule.getCharge() / (distance * distance);
-                Point2D vecteurChamp = direction.normalize().multiply(E);
-                champTotal = champTotal.add(vecteurChamp);
-            }
-        }
-        
-        return champTotal;
-    }
-
-    // Dessine la visualisation du champ électrique sur une grille
-    public static void dessinerVisualisationChamp(
-            List<ParticuleChargee> particules,
-            double LARGEUR_NIVEAU,
-            double HAUTEUR_ECRAN,
-            double largeurEcran,
-            Camera camera,
-            GraphicsContext contexteGraphique) {
-        
-        for (double x = 0; x < LARGEUR_NIVEAU; x += 50) {
-            for (double y = 0; y < HAUTEUR_ECRAN; y += 50) {
-                var positionMonde = new Point2D(x, y);
-                var positionEcran = camera.coordoEcran(positionMonde);
-                
-                // Seulement faire ça si la position (x, y) est visible dans l'écran
-                if (positionEcran.getX() >= 0 && positionEcran.getX() < largeurEcran &&
-                    positionEcran.getY() >= 0 && positionEcran.getY() < HAUTEUR_ECRAN) {
-                    
-                    Point2D force = champElectrique(particules, positionMonde);
-                    
-                    UtilitairesDessins.dessinerVecteurForce(
-                            positionEcran,
-                            force,
-                            contexteGraphique
-                    );
-                }
-            }
-        }
     }
 }
